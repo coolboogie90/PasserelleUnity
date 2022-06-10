46de34a4ba04cf3a183c9eb63ee9a2994998c845
Naïké Dramé <98624859+coolboogie90@users.noreply.github.com>
2022-06-10 16:21:30 +0200

    Created new repo

---
+++ Assets/Scripts/MoveDown.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    [Tooltip("Move Down.")]
+
+    public float speed = 1f;
+    private float factor = -1f;
+    
+    public GameObject maxYPosition;
+    public GameObject minYPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed * factor, 0) * Time.deltaTime;
+        
+        if(minYPosition != null && transform.position.y <= minYPosition.transform.position.y)
+        {
+            factor = 1f;            //inverse la position en y quand on arrive à 0 => après sa chute, l'objet remonte
+        }
+        else if(maxYPosition != null && transform.position.y >= maxYPosition.transform.position.y)
+        {
+            factor = -1f;
+        }
+    }
+}
+// Ne pas oublier d'assigner une target dans Unity
