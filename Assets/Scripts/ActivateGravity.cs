d727edf173ce0bbe4fa6842157c3bccc9b000e12
CoolBoogie90 <98624859+coolboogie90@users.noreply.github.com>
2022-06-24 13:52:17 +0200

    Added Input and Activate Script on capsule

--- Assets/Scripts/ActivateGravity.cs
+++ Assets/Scripts/ActivateGravity.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class ActivateGravity : MonoBehaviour
 {
+    Rigidbody rb; 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         InputDetector detector =  GetComponent<InputDetector>(); //on crée une variable contenant une instance d'une classe puis on lui dit "Va me chercher un objet de TYPE InputDetector (toute la class)
         detector.onInputDetected.AddListener(StartFalling); // On va creuser encore plus : après avoir cherché notre InputDetector(), on va trouver son UnityEvent onInputDetected
         //Equivalent à :
         //  GetComponent<InputDetector>().onInputDetected.AddListener(StartFalling)
+        rb = GetComponent<Rigidbody>();
         
     }
 
-    void StartFalling(){
-        Debug.Log("GERONIMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOooooooo.....");
+    public void StartFalling(){
+        rb.useGravity = true;
+        Debug.Log("GERONIMOOOOOOOOOOOOOOOOOOooooooooooooooooooo.........");
     }
 }
